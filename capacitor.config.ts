15bdb23867ca029d104cc32a0661e777bd5b2c0c
khushal <thabigbull9516@gmail.com>
2026-09-20 21:32:29 +0530

    align expo versions

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,8 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.vendor.app',
-  appName: 'vendor-app'
+  appName: 'vendor-app',
+  webDir: 'www'
 };
 
 export default config;
